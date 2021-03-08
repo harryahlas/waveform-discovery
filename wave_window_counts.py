740088cc99f9bda40cc3dc692026a0a79054a3f0
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-03-07 22:40:36 -0800

    import

--- wave_window_counts.py
+++ wave_window_counts.py
@@ -1,2 +1,39 @@
+# import required libraries 
+from pydub import AudioSegment  
+from pydub.playback import play  
+  
+# Import an audio file  
+# Format parameter only 
+# for readability  
+wav_file = AudioSegment.from_file(file = "data/KranK D w_ Mesa_289.wav",format = "wav")  
+  
+wav_file[0]
 
-"data/KranK D w_ Mesa_289.wav"
+
+# data type fo the file 
+print(type(wav_file))  
+
+#  To find frame rate of song/file 
+print(wav_file.frame_rate)    
+
+# To know about channels of file 
+print(wav_file.channels)  
+
+# Find the number of bytes per sample  
+print(wav_file.sample_width )  
+
+# Find Maximum amplitude  
+print(wav_file.max) 
+
+# To know length of audio file 
+print(len(wav_file)) 
+
+''' 
+We can change the attrinbutes of file by  
+changeed_audio_segment = audio_segment.set_ATTRIBUTENAME(x)  
+'''
+wav_file_new = wav_file.set_frame_rate(50)  
+print(wav_file_new.frame_rate) 
+
+
+x = wav_file.get_array_of_samples()
